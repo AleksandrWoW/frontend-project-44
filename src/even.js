39acf4fcb0c7fbcd306ff3320ebd9aa49d3f3dd8
AleksandrWoW <aleksandr.serow@mail.ru>
2024-05-06 13:55:05 +0300

    added

--- src/even.js
+++ src/even.js
@@ -1,21 +1,27 @@
 import readlineSync from 'readline-sync';
 
-    export default () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
+
+export default () => {
+    const nameUser = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${nameUser}!`);
+
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-    for (let i = 0; i < 3; i++){
-        if (number % 2 === 0 ) {
-        console.log(`Question: ${number} `);
-        readlineSync.question(`Your answer:  `);
-        } 
-        if (number % 2 !== 0 || '') {
-        console.log ("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!"); 
-        return ;
+    const randomMath = Math.floor(Math.random() * 101);
+    const even = randomMath % 2 === 0;
+    const evenOrOdd = even ? 'yes' : 'no';
+    
+    for (let i = 0; i < 3; i++) {
+        console.log(`Question: ${randomMath}`);
+        const youAnswer = readlineSync.question('Your answer: ');
+
+
+        if (youAnswer !== evenOrOdd) {
+            console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+            console.log(`Let's try again, ${nameUser}!`);
+            return;
         }
         console.log('Correct!');
-
-    };
-    console.log(`Congratulations, ${name}!`)
+    }
+    console.log(`Congratulations, ${nameUser}!`);
 };
