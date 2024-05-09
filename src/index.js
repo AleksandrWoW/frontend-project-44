2c7953cce253e708ccdaf8b2f9e17cb36a127209
AleksandrWoW <aleksandr.serow@mail.ru>
2024-05-09 10:45:59 +0300

    added

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+
+export default (randomMath) => {
+
+  console.log('Welcome to the Brain Games!');
+  const nameUser = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${nameUser}!`);
+
+  for (let i = 0; i < 3; i += 1) {
+    const random = randomMath();
+    const [even, evenOrOdd] = random;
+    console.log(`Question: ${even}`);
+    const youAnswer = readlineSync.question('Your answer: ');
+
+    if (youAnswer !== evenOrOdd) {
+      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+      console.log(`Let's try again, ${nameUser}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${nameUser}!`);
+
+};
--- src/logicGames/even.js
+++ src/logicGames/even.js
@@ -1,12 +1,5 @@
-import readlineSync from 'readline-sync';
-import randomNumber from './randomNumber.js';
-
-export default () => {
-  console.log('Welcome to the Brain Games!');
-  const nameUser = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${nameUser}!`);
-
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+import randomNumber from '../randomNumber.js';
+import startGames from '../index.js';
 
   const isEven = (number) => number % 2 === 0;
 
@@ -15,18 +8,7 @@ export default () => {
     const evenOrOdd = isEven(even) ? 'yes' : 'no';
     return [even, evenOrOdd];
   };
-  for (let i = 0; i < 3; i += 1) {
-    const roundData = randomMath();
-    const [even, evenOrOdd] = roundData;
-    console.log(`Question: ${even}`);
-    const youAnswer = readlineSync.question('Your answer: ');
-
-    if (youAnswer !== evenOrOdd) {
-      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-      console.log(`Let's try again, ${nameUser}!`);
-      return;
-    }
-    console.log('Correct!');
-  }
-  console.log(`Congratulations, ${nameUser}!`);
-};
+  
+  export default () => {
+    startGames(randomMath);
+  };
